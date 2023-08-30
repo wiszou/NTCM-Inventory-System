e71cacd6b48e100e7004547553536cb7df64b09c
wiszou <luis.santiago44.ls@gmail.com>
2023-08-30 16:22:20 +0800

    With delete button

--- NTCM-Inventory/resources/views/inventory.blade.php
+++ NTCM-Inventory/resources/views/inventory.blade.php
@@ -97,14 +97,71 @@
                             <span class="bg-blue-500 text-gray-50 rounded-xl px-2 py-1">Deployed</span>
                         </td>
                         <td class="pr-8 py-4 text-middle">
-                            <button
-                                class="flex p-1 bg-teal-700 rounded-xl hover:rounded-3xl hover:bg-teal-600 transition-all duration-300 text-white">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
-                                    stroke="currentColor" stroke-width="2">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                                </svg>
-                            </button>
+                            <div>
+                                <button onclick="openModal()"
+                                    class='bg-red-500 text-white p-2 rounded-xl text-2xl font-bold'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"> <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/> <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/> </svg>
+                                </button>
+                            </div>
+
+                            <div class="main-modal fixed w-full h-100 inset-0 z-50 overflow-hidden flex justify-center items-center animated fadeIn faster"
+                                style="background: rgba(0,0,0,.7);">
+                                <div
+                                    class="border border-teal-500 modal-container bg-white w-11/12 md:max-w-md mx-auto rounded-lg shadow-lg z-50 overflow-y-auto">
+                                    <div class="modal-content py-4 text-left px-6">
+                                        <!--Title-->
+                                        <div class="flex justify-between items-center pb-3">
+                                            <p class="text-2xl text-gray-700 font-bold w-full text-center">Delete Item</p>
+                                            <div class="modal-close cursor-pointer z-50">
+                                            </div>
+                                        </div>
+                                        <!--Body-->
+                                        <div class="mb-5 mt-3 text-center">
+                                            <p>Are you sure you want to delete this item?
+                                            </p>
+                                        </div>
+                                        <!--Footer-->
+                                        <div class="flex justify-center pt-2">
+                                            <button
+                                                class="focus:outline-none modal-close px-4 bg-gray-400 p-3 rounded-lg text-black hover:bg-gray-300">Cancel</button>
+                                            <button
+                                                class="focus:outline-none px-4 bg-red-500 p-3 ml-3 rounded-lg text-white hover:bg-red-700">Confirm</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <script>
+                            const modal = document.querySelector('.main-modal');
+                            const closeButton = document.querySelectorAll('.modal-close');
+
+                            const modalClose = () => {
+                                modal.classList.remove('fadeIn');
+                                modal.classList.add('fadeOut');
+                                setTimeout(() => {
+                                    modal.style.display = 'none';
+                                }, 500);
+                            }
+
+                            const openModal = () => {
+                                modal.classList.remove('fadeOut');
+                                modal.classList.add('fadeIn');
+                                modal.style.display = 'flex';
+                            }
+
+                            for (let i = 0; i < closeButton.length; i++) {
+
+                                const elements = closeButton[i];
+
+                                elements.onclick = (e) => modalClose();
+
+                                modal.style.display = 'none';
+
+                                window.onclick = function(event) {
+                                    if (event.target == modal) modalClose();
+                                }
+                            }
+                            </script>
                         </td>
                     </tr>
                 </tbody>
