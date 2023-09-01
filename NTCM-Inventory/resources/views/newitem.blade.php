f0562745930388301b118786a270574edabe6931
wiszou <102638597+wiszou@users.noreply.github.com>
2023-09-01 16:09:10 +0800

    Update Edit Modal

--- NTCM-Inventory/resources/views/newitem.blade.php
+++ NTCM-Inventory/resources/views/newitem.blade.php
@@ -222,10 +222,9 @@ td {
             <!--/container-->
 
 
-            <div class="main-modal fixed w-full h-100 inset-0 z-50 overflow-hidden flex justify-center items-center animated fadeIn faster"
+            <div class="main-modal fixed w-full h-100 inset-0 z-50 flex justify-center items-center animated fadeIn faster"
                 style="background: rgba(0,0,0,.7);">
-                <div
-                    class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded-xl shadow-lg z-50 overflow-y-auto">
+                <div class="modal-container bg-white w-3/6 rounded-xl z-50 overflow-y-auto">
                     <div class="modal-content py-4 text-left px-6">
                         <!--Title-->
                         <div class="flex justify-between items-center pb-3">
@@ -241,8 +240,8 @@ td {
                         </div>
                         <!--Body-->
                         <div class="flex justify-center">
-                            <div class="border py-4 rounded-lg w-full">
-                                <form action="#" class="relative bg-white rounded-lg shadow">
+                            <div class="border pt-4 pb-0 rounded-lg w-full">
+                                <form action="#" class="relative bg-white">
                                     <!-- Modal body -->
                                     <div class="p-6 space-y-6">
                                         <div class="grid grid-cols-6 gap-6">
@@ -266,14 +265,14 @@ td {
                                                 <label for="email"
                                                     class="block mb-2 text-sm font-medium text-gray-900 ">Item
                                                     Name:</label>
-                                                <input type="email" name="email" id="email"
+                                                <input type="text" name="email" id="email"
                                                     class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
                                                     placeholder="Laptop" required="">
                                             </div>
                                             <div class="col-span-6 sm:col-span-3">
                                                 <label for="phone-number"
                                                     class="block mb-2 text-sm font-medium text-gray-900 ">Category</label>
-                                                <input type="number" name="phone-number" id="phone-number"
+                                                <input type="text" name="phone-number" id="phone-number"
                                                     class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
                                                     placeholder="Device" required="">
                                             </div>
@@ -315,16 +314,55 @@ td {
                                             </div>
                                             <div class="col-span-6 sm:col-span-3">
                                                 <label for="current-password"
-                                                    class="block mb-2 text-sm font-medium text-gray-900 ">Status</label>
-                                              
-                                               
+                                                    class="block mb-2 text-sm font-medium text-gray-900 ">Remarks:</label>
+                                                <input type="Text" name="current-password" id="current-password"
+                                                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+                                                    placeholder="Returned by Ronald" required="">
+
                                             </div>
                                         </div>
                                         <!-- Modal footer -->
-                                        <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b">
-                                            <button type="submit"
-                                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Save
-                                                all</button>
+                                        <div
+                                            class="flex items-center pt-5 space-x-2 border-t border-gray-200 rounded-b">
+
+                                            <div class=" w-3/5">
+                                                <ul class="grid w-full grid-cols-3 gap-x-5 pr-4">
+                                                    <li class="">
+                                                        <input class="peer sr-only" type="radio" value="yes"
+                                                            name="answer" id="yes" checked />
+                                                        <label
+                                                            class="text-sm flex justify-center cursor-pointer rounded-full border border-gray-300 bg-white py-2 px-4 hover:bg-gray-50 focus:outline-none peer-checked:border-transparent peer-checked:ring-2 peer-checked:ring-green-500 peer-checked:bg-green-50 transition-all duration-200 ease-in-out"
+                                                            for="yes">Spare</label>
+                                                    </li>
+                                                    <li class="">
+                                                        <input class="peer sr-only" type="radio" value="no"
+                                                            name="answer" id="no" />
+                                                        <label
+                                                            class="text-sm flex justify-center cursor-pointer rounded-full border border-gray-300 bg-white py-2 px-4 hover:bg-gray-50 focus:outline-none peer-checked:border-transparent peer-checked:ring-2 peer-checked:ring-blue-500 peer-checked:bg-blue-50 transition-all duration-200 ease-in-out"
+                                                            for="no">Deployed</label>
+
+
+                                                    </li>
+
+                                                    <li class="">
+                                                        <input class="peer sr-only" type="radio" value="yesno"
+                                                            name="answer" id="yesno" />
+                                                        <label
+                                                            class="text-sm flex justify-center cursor-pointer rounded-full border border-gray-300 bg-white py-2 px-4 hover:bg-gray-50 focus:outline-none peer-checked:border-transparent peer-checked:ring-2 peer-checked:ring-orange-500 peer-checked:bg-orange-50 transition-all duration-200 ease-in-out "
+                                                            for="yesno">Borrowed</label>
+
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                            <div class=" w-2/5 flex justify-end">
+                                                <button type="submit"
+                                                    class="mr-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center">Update</button>
+                                                    <!--GANA LANG YUNG UPDATE BUTTON FOR STATUS SIGURO?-->
+                                                <button type="submit"
+                                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center">Save</button>
+                                                    
+
+                                            </div>
                                         </div>
                                 </form>
                             </div>
