1756db952d0601f06728661920a7e9f51a2f7774
KayL-1 <71209263+KayL-1@users.noreply.github.com>
2023-09-13 09:31:54 +0800

    1

--- NTCM-Inventory/resources/views/components/sidebar.blade.php
+++ NTCM-Inventory/resources/views/components/sidebar.blade.php
@@ -78,7 +78,7 @@
                 </ul>
             </li>
             <li>
-                <a href="{{ route('equipment') }}"
+                <a href="{{ route('updated-equipment') }}"
                     class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100 ">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
                         class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 "
