44173ac27e1be0c5b31d18746bcafc79c0256c07
wiszou <luis.santiago44.ls@gmail.com>
2023-09-29 15:01:59 +0800

    Supplier title

--- NTCM-Inventory/resources/views/suppliers.blade.php
+++ NTCM-Inventory/resources/views/suppliers.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Suppliers and Brands</title>
+    <title>Suppliers</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
