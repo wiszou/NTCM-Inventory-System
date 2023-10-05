5e3ead3e990277df0106e4b5d558b69c4b7e2d5b
wiszou <luis.santiago44.ls@gmail.com>
2023-10-05 09:57:11 +0800

    imployi

--- NTCM-Inventory/resources/views/employee.blade.php
+++ NTCM-Inventory/resources/views/employee.blade.php
@@ -144,21 +144,30 @@ input[disabled] {
                         <div class="col-span-6 sm:col-span-3">
                             <label for="item-serial" class="block mb-2 text-sm font-medium text-gray-900">Employee
                                 Name:</label>
-                            <input type="text" name="name" id="item-serial" class="shadow-sm  border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 editable-input" placeholder="Juan Dela Cruz" required="">
+                            <input type="text" name="name" id="item-serial"
+                                class="shadow-sm  border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 editable-input"
+                                placeholder="Juan Dela Cruz" required="">
                         </div>
 
                         <div class="col-span-6 sm:col-span-3">
-                            <label for="supplier-name" class="block mb-2 text-sm font-medium text-gray-900">Department:</label>
-                            <input type="text" name="department" id="item-model" class="shadow-sm  border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 editable-input" placeholder="IT Department" required="">
+                            <label for="supplier-name"
+                                class="block mb-2 text-sm font-medium text-gray-900">Department:</label>
+                            <input type="text" name="department" id="item-model"
+                                class="shadow-sm  border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 editable-input"
+                                placeholder="IT Department" required="">
                         </div>
 
                         <div class="col-span-6 sm:col-span-3">
-                            <label for="item-model" class="block mb-2 text-sm font-medium text-gray-900">Position:</label>
-                            <input type="text" name="position" id="item-model" class="shadow-sm  border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 editable-input" placeholder="IT Staff" required="">
+                            <label for="item-model"
+                                class="block mb-2 text-sm font-medium text-gray-900">Position:</label>
+                            <input type="text" name="position" id="item-model"
+                                class="shadow-sm  border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 editable-input"
+                                placeholder="IT Staff" required="">
                         </div>
 
                         <div class="col-span-6 sm:col-span-3 flex justify-end items-center mt-5 mr-2">
-                            <button type="submit" class="text-white bg-ntccolor hover:bg-teal-600 font-medium rounded-full px-5 h-10 mt-3 mb-3 text-sm text-center">Add
+                            <button type="submit"
+                                class="text-white bg-ntccolor hover:bg-teal-600 font-medium rounded-full px-5 h-10 mt-3 mb-3 text-sm text-center">Add
                                 Employee</button>
                         </div>
 
@@ -177,27 +186,35 @@ input[disabled] {
         <table id="example" class="stripe hover" style="width:100%; padding-top: 1em;  padding-bottom: 1em;">
             <thead class="">
                 <tr>
-                    <th data-priority="1">Employee Name</th>
-                    <th data-priority="2">Department</th>
-                    <th data-priority="3">Position</th>
-                    <th data-priority="3">Action</th>
+                    <th data-priority="1">Employee ID</th>
+                    <th data-priority="2">Employee Name</th>
+                    <th data-priority="3">Department</th>
+                    <th data-priority="4">Position</th>
+                    <th data-priority="5">Action</th>
                 </tr>
             </thead>
             <tbody id="suppliers">
                 @foreach ($employee as $item)
                 <tr>
+                <td class="text-center">IT-0001</td>
                     <td class="text-center">{{ $item->name }}</td>
                     <td class="text-center">{{ $item->department }}</td>
                     <td class="text-center">{{ $item->position }}</td>
                     <td class="text-center items-center flex justify-center">
-                        <button data-item-id="" onclick="openModal()" class="mr-1 btn btn-primary rounded-3xl text-ntccolor border border-ntccolor hover:bg-ntccolor hover:text-white font-medium text-sm p-1.5 text-center inline-flex items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="edit" class="w-5" fill="currentcolor">
-                                <path d="M5,18H9.24a1,1,0,0,0,.71-.29l6.92-6.93h0L19.71,8a1,1,0,0,0,0-1.42L15.47,2.29a1,1,0,0,0-1.42,0L11.23,5.12h0L4.29,12.05a1,1,0,0,0-.29.71V17A1,1,0,0,0,5,18ZM14.76,4.41l2.83,2.83L16.17,8.66,13.34,5.83ZM6,13.17l5.93-5.93,2.83,2.83L8.83,16H6ZM21,20H3a1,1,0,0,0,0,2H21a1,1,0,0,0,0-2Z">
+                        <button data-item-id="" onclick="openModal()"
+                            class="mr-1 btn btn-primary rounded-3xl text-ntccolor border border-ntccolor hover:bg-ntccolor hover:text-white font-medium text-sm p-1.5 text-center inline-flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="edit" class="w-5"
+                                fill="currentcolor">
+                                <path
+                                    d="M5,18H9.24a1,1,0,0,0,.71-.29l6.92-6.93h0L19.71,8a1,1,0,0,0,0-1.42L15.47,2.29a1,1,0,0,0-1.42,0L11.23,5.12h0L4.29,12.05a1,1,0,0,0-.29.71V17A1,1,0,0,0,5,18ZM14.76,4.41l2.83,2.83L16.17,8.66,13.34,5.83ZM6,13.17l5.93-5.93,2.83,2.83L8.83,16H6ZM21,20H3a1,1,0,0,0,0,2H21a1,1,0,0,0,0-2Z">
                                 </path>
                             </svg></button>
-                        <a href="#" data-brand-id="" class="brand-delete-link text-red-700 border border-red-700 hover:bg-red-700 hover:text-white font-medium rounded-full text-sm p-2   text-center inline-flex items-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:focus:ring-red-800 dark:hover:bg-red-500">
-                            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" class="w-4" fill="currentcolor" viewBox="0 0 16 16">
-                                <path d="M 6.496094 1 C 5.675781 1 5 1.675781 5 2.496094 L 5 3 L 2 3 L 2 4 L 3 4 L 3 12.5 C 3 13.328125 3.671875 14 4.5 14 L 10.5 14 C 11.328125 14 12 13.328125 12 12.5 L 12 4 L 13 4 L 13 3 L 10 3 L 10 2.496094 C 10 1.675781 9.324219 1 8.503906 1 Z M 6.496094 2 L 8.503906 2 C 8.785156 2 9 2.214844 9 2.496094 L 9 3 L 6 3 L 6 2.496094 C 6 2.214844 6.214844 2 6.496094 2 Z M 5 5 L 6 5 L 6 12 L 5 12 Z M 7 5 L 8 5 L 8 12 L 7 12 Z M 9 5 L 10 5 L 10 12 L 9 12 Z">
+                        <a href="#" data-brand-id=""
+                            class="brand-delete-link text-red-700 border border-red-700 hover:bg-red-700 hover:text-white font-medium rounded-full text-sm p-2   text-center inline-flex items-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:focus:ring-red-800 dark:hover:bg-red-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" class="w-4" fill="currentcolor"
+                                viewBox="0 0 16 16">
+                                <path
+                                    d="M 6.496094 1 C 5.675781 1 5 1.675781 5 2.496094 L 5 3 L 2 3 L 2 4 L 3 4 L 3 12.5 C 3 13.328125 3.671875 14 4.5 14 L 10.5 14 C 11.328125 14 12 13.328125 12 12.5 L 12 4 L 13 4 L 13 3 L 10 3 L 10 2.496094 C 10 1.675781 9.324219 1 8.503906 1 Z M 6.496094 2 L 8.503906 2 C 8.785156 2 9 2.214844 9 2.496094 L 9 3 L 6 3 L 6 2.496094 C 6 2.214844 6.214844 2 6.496094 2 Z M 5 5 L 6 5 L 6 12 L 5 12 Z M 7 5 L 8 5 L 8 12 L 7 12 Z M 9 5 L 10 5 L 10 12 L 9 12 Z">
                                 </path>
                             </svg>
                         </a>
@@ -210,15 +227,18 @@ input[disabled] {
     <!--/Card-->
 
 
-    <div class="main-modal fixed w-full h-100  inset-0 z-50 flex justify-center items-center animated fadeIn faster" style="background: rgba(0,0,0,.7);">
+    <div class="main-modal fixed w-full h-100  inset-0 z-50 flex justify-center items-center animated fadeIn faster"
+        style="background: rgba(0,0,0,.7);">
         <div class="modal-container bg-white w-3/6 rounded-xl z-50">
             <div class="modal-content py-4 text-left px-6 max-h-screen overflow-y-auto">
                 <!--Title-->
                 <div class="flex justify-between items-center pb-3">
                     <p class="text-xl font-semibold text-gray-700 mb-2" name="title" id="title">Edit Employee</p>
                     <div class="modal-close cursor-pointer z-50">
-                        <svg class="fill-current text-black" id="exitButton" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
-                            <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z">
+                        <svg class="fill-current text-black" id="exitButton" xmlns="http://www.w3.org/2000/svg"
+                            width="18" height="18" viewBox="0 0 18 18">
+                            <path
+                                d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z">
                             </path>
                         </svg>
                     </div>
@@ -232,21 +252,30 @@ input[disabled] {
                             <div class="col-span-6 sm:col-span-3">
                                 <label for="item-serial" class="block mb-2 text-sm font-medium text-gray-900">Employee
                                     Name:</label>
-                                <input type="text" name="name" id="item-serial" class="shadow-sm  border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 editable-input" placeholder="Juan Dela Cruz" required="">
+                                <input type="text" name="name" id="item-serial"
+                                    class="shadow-sm  border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 editable-input"
+                                    placeholder="Juan Dela Cruz" required="">
                             </div>
 
                             <div class="col-span-6 sm:col-span-3">
-                                <label for="supplier-name" class="block mb-2 text-sm font-medium text-gray-900">Department:</label>
-                                <input type="text" name="department" id="item-model" class="shadow-sm  border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 editable-input" placeholder="IT Department" required="">
+                                <label for="supplier-name"
+                                    class="block mb-2 text-sm font-medium text-gray-900">Department:</label>
+                                <input type="text" name="department" id="item-model"
+                                    class="shadow-sm  border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 editable-input"
+                                    placeholder="IT Department" required="">
                             </div>
 
                             <div class="col-span-6 sm:col-span-3">
-                                <label for="item-model" class="block mb-2 text-sm font-medium text-gray-900">Position:</label>
-                                <input type="text" name="position" id="item-model" class="shadow-sm  border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 editable-input" placeholder="IT Staff" required="">
+                                <label for="item-model"
+                                    class="block mb-2 text-sm font-medium text-gray-900">Position:</label>
+                                <input type="text" name="position" id="item-model"
+                                    class="shadow-sm  border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 editable-input"
+                                    placeholder="IT Staff" required="">
                             </div>
 
                             <div class="col-span-6 sm:col-span-3 flex justify-end items-center mt-5 mr-2">
-                                <button type="submit" class="text-white bg-ntccolor hover:bg-teal-600 font-medium rounded-full px-5 h-10 mt-3 mb-3 text-sm text-center">Update</button>
+                                <button type="submit"
+                                    class="text-white bg-ntccolor hover:bg-teal-600 font-medium rounded-full px-5 h-10 mt-3 mb-3 text-sm text-center">Update</button>
                             </div>
 
 
@@ -273,92 +302,92 @@ input[disabled] {
     <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
     <script>
-        $(document).ready(function() {
+    $(document).ready(function() {
 
-            var table = $('#example').DataTable({
-                    responsive: true
+        var table = $('#example').DataTable({
+                responsive: true
+            })
+            .columns.adjust()
+            .responsive.recalc();
+    });
+    </script>
+
+    <script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const form = document.getElementById('employee-form');
+
+        form.addEventListener('submit', function(e) {
+            e.preventDefault(); // Prevent the default form submission
+
+            // Serialize form data
+            const formData = new FormData(form);
+
+            fetch('/addEmployee', {
+                    method: 'POST',
+                    body: formData,
+                    headers: {
+                        'X-CSRF-TOKEN': '{{ csrf_token() }}', // Add your CSRF token here
+                    },
                 })
-                .columns.adjust()
-                .responsive.recalc();
+                .then(response => response.json())
+                .then(data => {
+                    if (data.success) {
+                        alert(data.message);
+                    } else {
+                        // Handle errors (e.g., show error message)
+                        alert(data.message);
+                    }
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
         });
+    });
     </script>
 
     <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            const form = document.getElementById('employee-form');
+    // Define the modal and closeButton variables
+    const modal = document.querySelector('.main-modal');
+    const closeButton = document.querySelectorAll('.modal-close');
 
-            form.addEventListener('submit', function(e) {
-                e.preventDefault(); // Prevent the default form submission
+    // Function to close the modal
+    const modalClose = () => {
+        modal.classList.remove('fadeIn');
+        modal.classList.add('fadeOut');
+        setTimeout(() => {
+            modal.style.display = 'none';
+        }, 10); // Adjust the delay as needed
+    };
 
-                // Serialize form data
-                const formData = new FormData(form);
+    // Function to open the modal
+    const openModal = () => {
+        // Show the modal - no need to redefine 'modal' here
+        modal.classList.remove('fadeOut');
+        modal.classList.add('fadeIn');
+        modal.style.display = 'flex';
+    };
 
-                fetch('/addEmployee', {
-                        method: 'POST',
-                        body: formData,
-                        headers: {
-                            'X-CSRF-TOKEN': '{{ csrf_token() }}', // Add your CSRF token here
-                        },
-                    })
-                    .then(response => response.json())
-                    .then(data => {
-                        if (data.success) {
-                            alert(data.message);
-                        } else {
-                            // Handle errors (e.g., show error message)
-                            alert(data.message);
-                        }
-                    })
-                    .catch(error => {
-                        console.error('Error:', error);
-                    });
-            });
-        });
-    </script>
+    // Attach click event listeners to close buttons
+    for (let i = 0; i < closeButton.length; i++) {
+        const element = closeButton[i];
+        element.onclick = () => modalClose();
+    }
 
-    <script>
-        // Define the modal and closeButton variables
-        const modal = document.querySelector('.main-modal');
-        const closeButton = document.querySelectorAll('.modal-close');
+    // Get the button element by its ID
+    const openModalButton = document.getElementById('open-modal-button');
+    if (openModalButton) {
+        openModalButton.addEventListener('click', () => openModal());
+    }
 
-        // Function to close the modal
-        const modalClose = () => {
-            modal.classList.remove('fadeIn');
-            modal.classList.add('fadeOut');
-            setTimeout(() => {
-                modal.style.display = 'none';
-            }, 10); // Adjust the delay as needed
-        };
+    // Initially hide the modal
+    modal.style.display = 'none';
 
-        // Function to open the modal
-        const openModal = () => {
-            // Show the modal - no need to redefine 'modal' here
-            modal.classList.remove('fadeOut');
-            modal.classList.add('fadeIn');
-            modal.style.display = 'flex';
-        };
-
-        // Attach click event listeners to close buttons
-        for (let i = 0; i < closeButton.length; i++) {
-            const element = closeButton[i];
-            element.onclick = () => modalClose();
+    // Click outside the modal to close it
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modalClose();
         }
-
-        // Get the button element by its ID
-        const openModalButton = document.getElementById('open-modal-button');
-        if (openModalButton) {
-            openModalButton.addEventListener('click', () => openModal());
-        }
-
-        // Initially hide the modal
-        modal.style.display = 'none';
-
-        // Click outside the modal to close it
-        window.onclick = function(event) {
-            if (event.target === modal) {
-                modalClose();
-            }
-        };
+    };
     </script>
 
 
